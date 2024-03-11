e9fc0cd27dd6be032d9f846c20f3c881671ace79
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-03-11 19:09:56 +0100

    refactor invoices component

---
+++ app/ui/invoices/create-form/index.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { createInvoice } from '@/app/lib/actions';
+import { CustomerField } from '@/app/lib/definitions';
+import { Button } from '@/app/ui/button';
+import CustomerName from '@/app/ui/invoices/create-form/customer-name';
+import InvoiceAmount from '@/app/ui/invoices/create-form/invoice-amount';
+import InvoiceStatusField from '@/app/ui/invoices/create-form/invoice-status';
+import Link from 'next/link';
+
+export default function Form({ customers }: { customers: CustomerField[] }) {
+  return (
+    <form action={createInvoice}>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        <CustomerName customers={customers} errors={state.errors} />
+
+        <InvoiceAmount errors={state.errors} />
+
+        <InvoiceStatusField errors={state.errors} />
+      </div>
+      <div className="mt-6 flex justify-end gap-4">
+        <Link
+          href="/dashboard/invoices"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Cancel
+        </Link>
+        <Button type="submit">Create Invoice</Button>
+      </div>
+    </form>
+  );
+}
---
+++ app/ui/invoices/create-form/invoice-amount.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { State } from '@/app/lib/actions';
+import { CurrencyDollarIcon } from '@heroicons/react/24/outline';
+
+export default function InvoiceAmount({ errors }: { errors: State['errors'] }) {
+  return (
+    <div className="mb-4">
+      <label htmlFor="amount" className="mb-2 block text-sm font-medium">
+        Choose an amount
+      </label>
+      <div className="relative mt-2 rounded-md">
+        <div className="relative">
+          <input
+            id="amount"
+            name="amount"
+            type="number"
+            step="0.01"
+            placeholder="Enter USD amount"
+            className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+          />
+          <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+        </div>
+      </div>
+    </div>
+  );
+}
