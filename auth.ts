3f51cf8b9b461bf71a6c7c8a22aa4ab30702fe3e
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-03-15 18:40:40 +0100

    add prisma

--- auth.ts
+++ auth.ts
@@ -1,15 +1,31 @@
 import type { User } from '@/app/lib/definitions';
-import { sql } from '@vercel/postgres';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
+import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import { authConfig } from './auth.config';
 
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
+
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (user) return user;
   } catch (error) {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
@@ -23,6 +39,7 @@ const UserSchema = z.object({
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  adapter: PrismaAdapter(prisma),
   providers: [
     Credentials({
       async authorize(credentials) {
